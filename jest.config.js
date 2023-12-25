083732a8ebe564f14ae47dbee81001baa9ecbb5c
Ko Dookie <koh@odds.team>
2023-12-25 15:38:05 +0700

    add eslint

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
+/** @typedef {import('ts-jest/dist/types').InitialOptions} InitialOptions */
+const config /** @type {InitialOptions} */ = {
+  // Your Jest configuration goes here
+  // ...
   preset: "ts-jest",
   testEnvironment: "node",
 };
+
+/* eslint-disable */
+module.exports = config;
